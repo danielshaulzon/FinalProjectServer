a4e19f05c4b46b67ad9ba104277ae71049f45df9
Daniel Shaulzon <daniel.shaulzon@gmail.com>
2023-03-01 19:37:34 +0200

    yeeeet

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def index():
     return send_from_directory(app.static_folder, "index.html")
 
 def create_app():
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=3000)
 
 
 if __name__ == "__main__":
